63f9a1ef55cd1f9de89652aa68de2427b836766c
Budmonde Duinkharjav <budmonde@github.mit.edu>
2018-01-10 11:54:00 -0500

    Add Step2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer(function(req, res) {
+
+const app = express();
+
+app.get('/', function(req, res) {
   res.send('Hello World!');
-});
+});)
 
 const port = 3000;
+const server = http.Server(app);
 server.listen(port, function() {
   console.log('Server running on port: ' + port);
 });)
