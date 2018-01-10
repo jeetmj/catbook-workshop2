13797a5a38f233a7f125d50e0c54dc80ce10905f
Budmonde Duinkharjav <budmonde@github.mit.edu>
2018-01-10 12:04:23 -0500

    Add Step3

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.user('/static', express.static('public'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html', { root: 'src/views' });
+});
+
+app.get('/u/profile', function(req, res) {
+  res.sendFile('profile.html', { root: 'src/views' });
 });
 
 const port = 3000;
